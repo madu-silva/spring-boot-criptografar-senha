90dd808171c16c2fac33bb4d71c5582af3b8587e
Maria Eduarda <mariaeduarda050501@gmail.com>
2022-04-26 16:08:46 -0300

    Projeto criptografando senhas - Parte 3 finalizado

--- src/main/java/com/example/criptografia/CriptografiaApplication.java
+++ src/main/java/com/example/criptografia/CriptografiaApplication.java
@@ -3,6 +3,10 @@ package com.example.criptografia;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCrypt;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class CriptografiaApplication {
@@ -11,4 +15,9 @@ public class CriptografiaApplication {
         SpringApplication.run(CriptografiaApplication.class, args);
     }
 
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        return encoder;
+    }
 }
