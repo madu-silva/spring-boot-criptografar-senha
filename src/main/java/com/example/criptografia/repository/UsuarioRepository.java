ee7250aa660763d9e635081b826dc0a8417057b6
Maria Eduarda <mariaeduarda050501@gmail.com>
2022-04-26 10:34:16 -0300

    Projeto criptografando senhas - parte 1

---
+++ src/main/java/com/example/criptografia/repository/UsuarioRepository.java
@@ -0,0 +1,12 @@
+package com.example.criptografia.repository;
+
+import com.example.criptografia.model.UsuarioModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UsuarioRepository extends JpaRepository<UsuarioModel, Integer> {
+
+    public Optional<UsuarioModel> findByLogin(String login);
+
+}
